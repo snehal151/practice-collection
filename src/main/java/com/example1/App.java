1012f63914a7e27006242a02375327cb2b03751c
snehal151 <51915392+snehal151@users.noreply.github.com>
2019-09-22 16:02:36 +0100

    msg2 added

--- src/main/java/com/example1/App.java
+++ src/main/java/com/example1/App.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
-// it is main class
+// it is main class..........
 public class App {
     public static void main(String[] args) {
         List<Customer> customerList = new ArrayList<>();
