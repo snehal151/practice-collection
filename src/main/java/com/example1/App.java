d5a95c35117451d6099e2d01f3509362b41cd0a8
Sneha <snehal.gavali27@gmail.com>
2019-09-24 22:26:30 +0100

    Added logger

--- src/main/java/com/example1/App.java
+++ src/main/java/com/example1/App.java
@@ -1,6 +1,8 @@
 package com.example1;
 
 import com.example1.info.Customer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -8,99 +10,131 @@ import java.util.List;
 import java.util.Scanner;
 // it is main class..........
 public class App {
+    private static Logger logger = LoggerFactory.getLogger(App.class);
+    
     public static void main(String[] args) {
         List<Customer> customerList = new ArrayList<>();
         int choice;
         Scanner s= new Scanner(System.in);
         do {
-            System.out.println("1. Insert");
-            System.out.println("2. Display one");
-            System.out.println("3. Display all");
-            System.out.println("4. Delete ");
-            System.out.println("5. Update");
-            System.out.println("6. Exit");
-            System.out.println("Enter your choice");
+            logger.info("1. Insert");
+            logger.info("2. Display one");
+            logger.info("3. Display all");
+            logger.info("4. Delete ");
+            logger.info("5. Update");
+            logger.info("6. Exit");
+            logger.info("Enter your choice");
 
             choice = s.nextInt();
 
             switch (choice) {
                 case 1:
-                    System.out.println("Enter Customer information:");
-                    System.out.println("Id:");
-                    int tId = s.nextInt();
-                    System.out.println("Name:");
-                    String tName = s.next();
-                    System.out.println("City");
-                    String tCity= s.next();
-                    Customer c = new Customer(tId,tName,tCity);
-                    customerList.add(c);
+                    Customer customer1 = getNewCustomer(s);
+                    customerList.add(customer1);
                     break;
                 case 2:
-                    System.out.println("Enter customer id : ");
-                    int searchbyid = s.nextInt();
-                    Iterator<Customer> iterator1 = customerList.iterator();
-                    while (iterator1.hasNext()){
-                        Customer customer = iterator1.next();
-                        if(customer.getId()==searchbyid){
-                           // customerList.remove(customer);
-                           System.out.println(customer);
-                        }
+                    logger.info("Enter customer id : ");
+                    int idToSearch = s.nextInt();
 
+                    if(isExist(idToSearch,customerList)==true) {
+                    printCustomer(idToSearch, customerList);
+                } else {
+                        logger.error("Provided id {} not found!", idToSearch);
                     }
-                    //System.out.println("Record deleted");
                 break;
                 case 3:
-                    System.out.println("Customer List");
-                    Iterator<Customer> iterator = customerList.iterator();
-                    while (iterator.hasNext()) {
-                        Customer customer = iterator.next();
-                        System.out.println(customer);
-                    }
+                    printCustomerList(customerList);
                 break;
                 case 4:
-                    System.out.println("Enter customer id : ");
-                    int searchid = s.nextInt();
-                    Iterator<Customer> iterator2 = customerList.iterator();
-                    while (iterator2.hasNext()){
-                        Customer customer = iterator2.next();
-                        if(customer.getId()==searchid){
-                            customerList.remove(customer);
-                            // System.out.println(customer);
-                        }
+                    logger.info("Enter customer id : ");
+                    idToSearch = s.nextInt();
 
+                    if (isExist(idToSearch , customerList) == true) {
+                        removeCustomer(idToSearch, customerList);
+                    } else {
+                        logger.error("Provided id {} not found!", idToSearch);
                     }
-                    //System.out.println("Record deleted");
-                    break;
+                 break;
                 case 5:
-                    System.out.println("Enter customer id : ");
-                    int searchforid = s.nextInt();
-                    Iterator<Customer> iterator3 = customerList.iterator();
-
-                    while (iterator3.hasNext()){
-                        Customer customer = iterator3.next();
-                        if(customer.getId()==searchforid){
-                            //customerList.remove(customer);
-                            // System.out.println(customer);
-                            System.out.println("Enter name to be update");
-                            String tsName = s.next();
-                            customer.setName(tsName);
-                            System.out.println("Enter city to be update");
-                            String tsCity = s.next();
-                            customer.setCity(tsCity);
-                            System.out.println("Record Updated");
-                        }
-                       // System.out.println("ID Not Found");
-
+                    logger.info("Enter customer id : ");
+                    idToSearch = s.nextInt();
+                    if (isExist(idToSearch , customerList) == true) {
+                        updateCustomer(idToSearch, customerList,s);
+                    } else {
+                        logger.error("Provided id {} not found!", idToSearch);
                     }
-
-                    break;
+                 break;
                 case 6:
                     break;
                 default:
-                    System.out.println("Please enter correct choice");
+                    logger.info("Please enter correct choice");
                     break;
             }
 
         } while (choice != 6);
+    }
+
+    private static void updateCustomer(int idToSearch, List<Customer> customerList,Scanner s) {
+        Iterator<Customer> iterator3 = customerList.iterator();
+
+        while (iterator3.hasNext()){
+            Customer customer = iterator3.next();
+            if(customer.getId()==idToSearch){
+                logger.info("Enter name to be update");
+                String tsName = s.next();
+                customer.setName(tsName);
+                logger.info("Enter city to be update");
+                String tsCity = s.next();
+                customer.setCity(tsCity);
+                logger.info("Record Updated");
+            }
+        }
+    }
+    private static void removeCustomer(int idToSearch, List<Customer> customerList) {
+        Iterator<Customer> iterator2 = customerList.iterator();
+        while (iterator2.hasNext()){
+            Customer customer = iterator2.next();
+            if(customer.getId()==idToSearch){
+                customerList.remove(customer);
+            }
+        }
+    }
+    private static void printCustomerList(List<Customer> customerList) {
+        logger.info("Customer List");
+        Iterator<Customer> iterator = customerList.iterator();
+        while (iterator.hasNext()) {
+            Customer customer = iterator.next();
+            logger.info(customer.toString());
+        }
+    }
+    private static void printCustomer(int idToSearch, List<Customer> customerList) {
+        Iterator<Customer> iterator1 = customerList.iterator();
+        while (iterator1.hasNext()){
+            Customer customer = iterator1.next();
+            if(customer.getId()==idToSearch){
+                // customerList.remove(customer);
+                logger.info(customer.toString());
+            }
+        }
+    }
+    private static boolean isExist(int idToSearch , List<Customer> customerList){
+        Iterator<Customer> iterator1 = customerList.iterator();
+        while (iterator1.hasNext()){
+            Customer customer = iterator1.next();
+            if(customer.getId()==idToSearch){
+                return true;
+            }
+        }
+        return false;
+    }
+    private static Customer getNewCustomer(Scanner s) {
+        logger.info("Enter Customer information:");
+        logger.info("Id:");
+        int tId = s.nextInt();
+        logger.info("Name:");
+        String tName = s.next();
+        logger.info("City");
+        String tCity= s.next();
+        return new Customer(tId,tName,tCity);
     }
 }
